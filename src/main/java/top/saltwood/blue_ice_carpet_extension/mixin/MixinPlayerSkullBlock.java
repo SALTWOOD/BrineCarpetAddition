26ef03d650e93127525aaa4c263c288149d29ef3
SaltWood_233 <105980161+SALTWOOD@users.noreply.github.com>
2026-01-07 22:19:16 +0900

    fix(deathskull): always drop inventory

--- src/main/java/top/saltwood/blue_ice_carpet_extension/mixin/MixinPlayerSkullBlock.java
+++ src/main/java/top/saltwood/blue_ice_carpet_extension/mixin/MixinPlayerSkullBlock.java
@@ -1,14 +1,16 @@
 package top.saltwood.blue_ice_carpet_extension.mixin;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.PlayerSkullBlock;
 import net.minecraft.block.SkullBlock;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.ExperienceOrbEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.stat.Stats;
+import net.minecraft.util.ItemScatterer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
@@ -24,26 +26,50 @@ public abstract class MixinPlayerSkullBlock extends SkullBlock {
     }
 
     @Override
-    public void afterBreak(@NotNull World level, PlayerEntity player, BlockPos pos, BlockState state, @Nullable BlockEntity blockEntity, ItemStack stack) {
-        if (level.isClient()) return;
-        if (blockEntity instanceof DeathSkullInterface) {
-            DeathInfo deathInfo = ((DeathSkullInterface) blockEntity).deathInfo$get();
-            if (deathInfo == null) {
-                super.afterBreak(level, player, pos, state, blockEntity, stack);
-            } else {
-                player.incrementStat(Stats.MINED.getOrCreateStat(this));
-                player.addExhaustion(0.005F);
-                for (ItemStack itemStack : deathInfo.inventory.clearToList()) {
-                    Block.dropStack(level, pos, itemStack);
-                }
+    protected void onStateReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved) {
+        if (state.isOf(newState.getBlock())) {
+            super.onStateReplaced(state, world, pos, newState, moved);
+            return;
+        }
 
-                int xp = deathInfo.exp;
-                while (xp > 0) {
-                    int spawnedXp = ExperienceOrbEntity.roundToOrbSize(xp);
-                    xp -= spawnedXp;
-                    level.spawnEntity(new ExperienceOrbEntity(level, pos.getX(), pos.getY(), pos.getZ(), spawnedXp));
+        if (!world.isClient && world instanceof ServerWorld serverWorld) {
+            BlockEntity blockEntity = serverWorld.getBlockEntity(pos);
+            if (!(blockEntity instanceof DeathSkullInterface skull)) return;
+
+            DeathInfo deathInfo = skull.deathInfo$get();
+            if (deathInfo == null) return;
+
+            // 1. Items
+            ItemScatterer.spawn(serverWorld, pos, deathInfo.inventory);
+
+            // 2. Experience orbs
+            int xp = deathInfo.exp;
+            while (xp > 0) {
+                int spawnedXp = ExperienceOrbEntity.roundToOrbSize(xp);
+                xp -= spawnedXp;
+                serverWorld.spawnEntity(new ExperienceOrbEntity(serverWorld, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, spawnedXp));
+            }
+
+            // 3. TextDisplay
+            if (deathInfo.display != null) {
+                Entity display = serverWorld.getEntity(deathInfo.display);
+                if (display != null) {
+                    display.discard();
                 }
             }
         }
+
+        super.onStateReplaced(state, world, pos, newState, moved);
+    }
+
+    @Override
+    public void afterBreak(@NotNull World world, PlayerEntity player, BlockPos pos, BlockState state, @Nullable BlockEntity blockEntity, ItemStack stack) {
+        if (blockEntity instanceof DeathSkullInterface skull && skull.deathInfo$get() != null) {
+            player.incrementStat(Stats.MINED.getOrCreateStat(this));
+            player.addExhaustion(0.005F);
+            return;
+        }
+
+        super.afterBreak(world, player, pos, state, blockEntity, stack);
     }
 }
